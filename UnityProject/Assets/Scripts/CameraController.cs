a99f53fee15ec2bc1fed702f4d74d5d5d5319c8b
Toshiyuki Higa <toshiyukihiga@Toshiyuki-no-MacBook-Pro.local>
2016-09-21 18:40:40 +0900

    no message

---
+++ UnityProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour 
+{
+	public void SetTankController(TankController iTankController) { mTankController = iTankController; }
+	private void Update()
+	{
+		if(mTankController == null) { return; }
+		TankTorres();
+	}
+
+	private void TankTorres()
+	{
+		Vector3 aTorresTransform;
+		aTorresTransform   = new Vector3(mTankController.transform.position.x,transform.position.y,transform.position.z);
+		transform.position = aTorresTransform;
+	}
+
+	private TankController mTankController;
+}
