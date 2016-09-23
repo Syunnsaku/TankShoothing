56be18b985c43ee480388363ce1aba9e6b0a5b48
Toshiyuki Higa <toshiyukihiga@Toshiyuki-no-MacBook-Pro.local>
2016-09-23 09:40:03 +0900

    インゲームか判定

---
+++ UnityProject/Assets/Scripts/Censer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Censer : MonoBehaviour 
+{
+	private void Awake()
+	{
+		mDetectionAlready = false;
+	}
+
+	private void Start()
+	{
+		mTankController = transform.parent.gameObject.GetComponent<TankController>();
+	}
+
+	private void OnTriggerStay(Collider iCollider)
+	{
+		mIsDetection = true;
+		if(iCollider.gameObject.layer == 9)
+		{
+			mDetectionAlready = true;
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		if(mIsDetection == true)
+		{
+			mIsDetection = false;
+			mTankController.BootAlert();
+		}
+		else
+		if(mIsDetection == false)
+		{
+			mTankController.TerminateAlert();
+		}
+
+	}
+
+	private TankController mTankController;
+	private bool           mDetectionAlready;
+	private bool           mIsDetection;
+}
