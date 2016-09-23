c941460796a8fe0c8d11317971c2d6c1b4f67162
Toshiyuki Higa <toshiyukihiga@Toshiyuki-no-MacBook-Pro.local>
2016-09-23 09:40:31 +0900

    ゲームの判定処理を追加

--- UnityProject/Assets/Scripts/GameManager.cs
+++ UnityProject/Assets/Scripts/GameManager.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class GameManager : Singleton<GameManager> 
 {
 	public void SetIsGaemInProgress(bool iIsGameInProgress) { mIsGameInProgress = iIsGameInProgress; }
+
 	protected override void Initialize()
 	{
+		mIsGameInProgress = true;
 	}
 		
 	public void GameOver()
 	{
+		if(mIsGameInProgress == false) { return; }
 		Menu_Main aMenuMain = UIManager.GetInstance().GetMenu("Menu_Main").GetComponent<Menu_Main>();
 		aMenuMain.GameOver();
 	}
